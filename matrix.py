3d11d8849d234c53f4cf5d96a54aec7ad2c29d2f
SheeyeDev <102896476+SheeyeDev@users.noreply.github.com>
2023-05-26 12:54:27 +0200

    Zmiana wierszy

--- matrix.py
+++ matrix.py
@@ -11,11 +11,30 @@ class Matrix():
         pass
 
     def findInverseMatrix(self):
+        self.checkSwaps()
         for x in range(len(self.matrix)):
             self.divideRow(x,1/self.matrix[x][x])
             for y in range(len(self.matrix)):
                 if y != x:
                     self.substractRow(y,x,self.matrix[y][x]/self.matrix[x][x])
+
+    def checkSwaps(self):
+        for x in range(len(self.matrix)):
+            if self.matrix[x][x]==0:
+                for y in range(len(self.matrix)):
+                    if self.matrix[y][x]!=0 and self.matrix[x][y] != 0:
+                        self.swapRows(x,y)
+                        break
+                    elif y==len(self.matrix)-1:
+                        print("Dla podanej macierzy nie da się ustalić macierzy odwrotnej.")
+                        exit()
+
+    def swapRows(self,swap1,swap2):
+        for x in range(len(self.matrix[swap1])):
+            tmp = self.matrix[swap1][x]
+            self.matrix[swap1][x]=self.matrix[swap2][x]
+            self.matrix[swap2][x] = tmp
+
     def printInverseMatrix(self):
         for x in range(len(self.inverse)):
             for y in range(len(self.inverse[x])):
